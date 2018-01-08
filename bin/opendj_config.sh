15d548eb3df7f3a974ed5aefd8f521f0d204985b
pesoklp13 <pesoklp13@gmail.com>
2018-01-08 20:53:26 +0100

    Update opendj_config.sh

--- bin/opendj_config.sh
+++ bin/opendj_config.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+cat <<EOF > /home/opendj/conf/basedn.ldif
+dn: dc=sk
+objectClass: domain
+objectClass: top
+dc: sk
+
+dn: dc=st,dc=sk
+objectClass: domain
+objectClass: top
+dc: st
+aci: (target ="ldap:///dc=st,dc=sk")(targetattr !=
+ "userPassword")(version 3.0;acl "Anonymous read-search access";
+ allow (read, search, compare)(userdn = "ldap:///anyone");)
+EOF
+
 cat <<EOF > /home/opendj/conf/opendj-install.properties
 hostname                        =$(hostname)
 ldapPort                        =${BASE_PORT}
@@ -11,6 +26,7 @@ adminConnectorPort              =${ROOT_PORT}
 rootUserDN                      =${ROOT_DN}
 rootUserPassword                =${ROOT_PWD}
 baseDN                          =${BASE_DN}
+ldifFile                        =/home/opendj/conf/basedn.ldif
 EOF
 
 cat /home/opendj/conf/opendj-install.properties
